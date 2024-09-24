97380a642a96d810bd45b23e3cfb91712f70717b
mfvicky <mfsi.vickys@gmail.com>
2024-09-24 11:04:43 +0530

    remove unused code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import QuillEditor from './components/QuillEditor';
-import QuillEditor1 from './components/QuillEditor1';
 
 function App() {
   return (
     <div className="App">
       <QuillEditor />
-      {/* <QuillEditor1 /> */}
     </div>
   );
 }
